0f52dfbeb159440e8af32e57ec4bde6d9de3fc3b
20261481 <jangan20261481@gmail.com>
2026-09-15 12:56:31 +0900

    c 변수추가

--- python/2주차.py
+++ python/2주차.py
@@ -8,16 +8,16 @@ print("30" "*" "30" "=",30 * 30)
 print("123" "*" "456" "=", 123*456)
 a = str(input("첫번째 값 :"))
 b = str(input("두번째 값 :"))
+c = str(input("세번째 값 :"))
 ## result = a*b
 ## result1 = a/b
 ## result2 = a+b
 ## result3 = a-b
 
 ## print(a, "*", b, "=", result)
-print(a,"and",b)
 ##print(a, "/", b, "=", result1)
 ##print(a, "+", b, "=", result2)
 ##print(a, "-", b, "=", result3)
-
+print(a,"and",b,"and",c)
 
       
