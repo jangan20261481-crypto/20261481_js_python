fa6e8af73d983a2232da06b6bae279bf232e9a8a
20261481 <jangan20261481@gmail.com>
2026-09-15 13:49:51 +0900

    3주차파일추가

---
+++ python/3주차.py
@@ -0,0 +1,14 @@
+##a = int(input("첫번째 값 :"))
+##b = int(input("두번째 값 :"))
+##c = int(input("세번째 값 :"))
+
+
+##print(a, "*", b, "=", a*b)
+##print(a, "+", b, "=", a+b)
+##print(a, "-", b, "=", a-b)
+##print(a, "/", b, "=," a/b)
+
+data="안녕" + \
+"하세요?" + \
+"파이썬!"
+print(data)
